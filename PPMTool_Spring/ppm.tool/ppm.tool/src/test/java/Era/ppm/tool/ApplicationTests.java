69cf030ebfc38a8773b2e773958042e1e85d00fa
Christy <Edgardchristianne@gmail.com>
2024-11-14 16:18:46 -0500

    firt commit

---
+++ PPMTool_Spring/ppm.tool/ppm.tool/src/test/java/Era/ppm/tool/ApplicationTests.java
@@ -0,0 +1,13 @@
+package Era.ppm.tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
